03c13239a0b9ba807156ade334d70ed3c9b04727
AkshitRanjan <73226453+AkshitRanjan@users.noreply.github.com>
2021-01-13 17:44:17 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -16,6 +16,9 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
       //End the code
 });});}
 
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
 function addRoom()
 {
   room_name = document.getElementById("room_name").value;
